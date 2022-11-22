2fa20b9a004088b7e36639f1f6e848eb59d30ff2
Jose Vicente González Pérez <j.gonzalez2299@gmail.com>
2022-11-22 16:58:41 -0300

    delete something

--- app/controllers/ProductosController.php
+++ app/controllers/ProductosController.php
@@ -4,95 +4,10 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use \App\models\Producto as Producto;
 
-require_once './models/PDF.php';
 require_once './models/Producto.php';
 require_once './interfaces/IApiUsable.php';
 class ProductosController implements IApiUsable
 {
-  // public function DescargaPDF(Request $request, Response $response, array $args)
-  // {
-  //   try {
-  //     ob_start();
-  //     $pdf = new PDF();
-  //     $productos = Producto::all();
-  //     $pdf->AliasNbPages();
-  //     $pdf->AddPage();
-  //     $pdf->SetFont('Times', '', 12);
-  //     foreach ($productos as $prod) {
-  //       $pdf->Body($this->ToCvs($prod));
-  //     }
-  //     $pdf->Output();
-  //     ob_end_flush();
-  //     $payload = json_encode(array("Resultado" => "Descargado"));
-  //     $response->getBody()->write($payload);
-
-  //     return $response
-  //       ->withHeader('Content-Type', 'application/csv')
-  //       ->withStatus(200);
-  //   } catch (Exception $ex) {
-  //     $error = $ex->getMessage();
-  //     $datosError = json_encode(array("Error" => $error));
-  //     $response->getBody()->write($datosError);
-  //     return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
-  //   }
-  // }
-  // public function CargaCSV(Request $request, Response $response, array $args)
-  // {
-  //   try {
-  //     if ($request->getUploadedFiles()) {
-  //       $archivo = $request->getUploadedFiles();
-  //       $destino = __DIR__;
-  //       $nombreAnterior = $archivo['archivo']->getClientFileName();
-  //       $extension = explode(".", $nombreAnterior);
-  //       $extension = array_reverse($extension)[0];
-  //       if ($extension != "csv") {
-  //         throw new Exception("El tipo de foto es incorrecto");
-  //       }
-  //       $pathArchivo = $destino . "." . date("Gis");
-  //       $archivo = $archivo['archivo'];
-  //       $archivo->moveTo($pathArchivo);
-
-
-  //       $archivoAbierto = fopen($pathArchivo, 'r');
-  //       $arrayObjetos = array();
-  //       if ($archivoAbierto != null) {
-  //         while (!feof($archivoAbierto)) {
-  //           $aux = fgets($archivoAbierto);
-  //           $lectura = explode(',', $aux);
-  //           if (
-  //             isset($lectura[0]) && !empty($lectura[0]) &&
-  //             isset($lectura[1]) && !empty($lectura[1]) &&
-  //             isset($lectura[2]) && !empty($lectura[2]) &&
-  //             isset($lectura[3]) && !empty($lectura[3]) &&
-  //             isset($lectura[4]) && !empty($lectura[4])
-  //           ) {
-  //             $prodGenerico = (object)[
-  //               "Codigo" => $lectura[0], "TipoProductoId" => $lectura[1],
-  //               "Nombre" => $lectura[2], "Stock" => $lectura[3], "Precio" => $lectura[4]
-  //             ];
-  //             array_push($arrayObjetos, $prodGenerico);
-  //           }
-  //         }
-  //         fclose($archivoAbierto);
-  //         unlink($pathArchivo);
-  //         if (count($arrayObjetos) > 0) {
-  //           $this->ActualizarProductos($arrayObjetos);
-  //           $payload = json_encode(array("Resultado" => "Agregado"));
-  //           $response->getBody()->write($payload);
-  //         }
-  //       }
-  //       return $response
-  //         ->withHeader('Content-Type', 'application/csv')
-  //         ->withStatus(200);
-  //     }
-  //     throw new Exception("No se subio ningun archivo");
-  //   } catch (Exception $ex) {
-  //     $error = $ex->getMessage();
-  //     $datosError = json_encode(array("Error" => $error));
-  //     $response->getBody()->write($datosError);
-  //     return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
-  //   }
-  // }
   private function ActualizarProductos($listaNueva)
   {
     if (isset($listaNueva)) {
@@ -136,40 +51,9 @@ class ProductosController implements IApiUsable
   {
     return $prod1->Codigo == $prod2->Codigo;
   }
-  // public function DescargaCSV(Request $request, Response $response, array $args)
-  // {
-  //   $ruta = __DIR__ . date("Gis") . "productos.csv";
-  //   try {
-  //     $archivo = fopen($ruta, "a");
-  //     if ($archivo != null) {
-  //       $productos = Producto::all();
-  //       foreach ($productos as $prod) {
-  //         fwrite($archivo, $this->ToCvs($prod));
-  //       }
-  //       fclose($archivo);
-  //     }
-  //     if (readfile($ruta)) {
-  //       unlink($ruta);
-  //       return $response
-  //         ->withHeader('Content-Type', 'application/csv')
-  //         ->withStatus(200);
-  //     }
-  //   } catch (Exception $ex) {
-  //     $error = $ex->getMessage();
-  //     $datosError = json_encode(array("Error" => $error));
-  //     $response->getBody()->write($datosError);
-  //     return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
-  //   }
-  // }
-  // private function ToCvs($producto)
-  // {
-  //   $datos = "$producto->Codigo,$producto->TipoProductoId,$producto->Nombre,$producto->Stock,$producto->Precio,$producto->FechaCreacion,$producto->FechaUltimaModificacion,$producto->Eliminado\n";
-  //   return $datos;
-  // }
   public function TraerUno(Request $request, Response $response, array $args)
   {
     try {
-      //Los datos ingresados por la url se buscan en args
       $id = $args["id"];
       $producto = Producto::where('Id', '=', $id)->first();
       if (is_null($producto)) {
@@ -206,7 +90,6 @@ class ProductosController implements IApiUsable
   {
     try {
       $datosIngresados = $request->getParsedBody()["body"];
-      //Validación de datosIngresados
       if (
         !isset($datosIngresados["codigo"])
         || !isset($datosIngresados["tipoProductoId"])
